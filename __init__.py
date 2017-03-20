a3947122fbb0f11e58addc4bcff6b35c89c527b1
Ryan <tripper@TripperdeMacBook-Pro.local>
2017-03-20 13:18:19 +0800

    fix add button dissapear bug in change_list.html

---
+++ __init__.py
@@ -0,0 +1,3 @@
+
+
+default_app_config = 'payment_app.apps.PaymentAppConfig'
--- apps.py
+++ apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class PaymentAppConfig(AppConfig):
     name = 'payment_app'
+    verbose_name='PAYMENT'
